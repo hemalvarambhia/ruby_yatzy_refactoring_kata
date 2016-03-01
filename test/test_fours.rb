b31a9d5470fbf596178359231267d72f348da214
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-01 21:22:47 +0000

    Created a dedicated test case for the fours method

---
+++ test/test_fours.rb
@@ -0,0 +1,15 @@
+require_relative 'test_helper'
+require 'yatzy'
+
+class TestFours < Test::Unit::TestCase
+  def test_that_a_throw_with_no_4s_yields_0_score
+    assert_equal 0, Yatzy.new(1, 2, 3, 5, 6).fours
+  end
+    
+  def test_that_a_throw_with_4s_yields_a_sum_of_only_the_4s
+    assert_equal 12, Yatzy.new(4, 4, 4, 5, 5).fours
+    assert_equal 8, Yatzy.new(4, 4, 5, 5, 5).fours
+    assert_equal 4, Yatzy.new(4, 5, 5, 5, 5).fours
+    assert_equal 16, Yatzy.new(4, 4, 4, 4, 1).fours
+  end
+end
