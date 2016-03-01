e36a26cd05f8f23f29eba0a51245053f4b3ac82e
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-01 21:13:10 +0000

    Improved the method name

--- test/test_twos.rb
+++ test/test_twos.rb
@@ -6,11 +6,11 @@ class TestTwos < Test::Unit::TestCase
     assert_equal 0, Yatzy.twos(1, 1, 3, 1, 6)
   end
 
-  def test_that_a_throw_with_2s_yields_a_score_that_their_sum
+  def test_that_a_throw_with_2s_yields_the_sum_of_only_the_2s
     assert_equal 2, Yatzy.twos(2, 1, 3, 4, 6)
     assert_equal 4, Yatzy.twos(2, 2, 1, 3, 3)
     assert_equal 6, Yatzy.twos(2, 2, 2, 3, 3)
-     assert_equal 8, Yatzy.twos(2, 2, 2, 2, 3)
+    assert_equal 8, Yatzy.twos(2, 2, 2, 2, 3)
     assert_equal 10, Yatzy.twos(2, 2, 2, 2, 2)
   end
 end
