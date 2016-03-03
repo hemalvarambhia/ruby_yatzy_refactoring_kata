9ad0e33fcc7d7fdd131482ace43ee9026522666f
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-03 13:23:35 +0000

    Added tests for yatzy where faces are different. This is to ensure we catch bugs

--- test/test_yatzy.rb
+++ test/test_yatzy.rb
@@ -2,10 +2,12 @@ require_relative 'test_helper'
 
 class YatzyTest < Test::Unit::TestCase
   def test_yatzy_scores_50
-    assert_equal 50, Yatzy.yatzy([6,6,6,6,6])
+    assert_equal 50, Yatzy.yatzy([6, 6, 6, 6, 6])
+    assert_equal 50, Yatzy.yatzy([2, 2, 2, 2, 2])
+    assert_equal 50, Yatzy.yatzy([3, 3, 3, 3, 3])
   end
 
   def test_no_yatzy_scores_0
-    assert_equal 0, Yatzy.yatzy([6,6,6,6,3])
+    assert_equal 0, Yatzy.yatzy([6, 6, 6, 6, 3])
   end
 end
