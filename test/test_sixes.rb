6f542ba9a1adba589e0dfb41832fed3f6e077cef
Hemal <h.n.varambhia@gmail.com>
2016-03-08 12:41:30 +0000

    Extracted an invocation method

--- test/test_sixes.rb
+++ test/test_sixes.rb
@@ -2,14 +2,18 @@ require 'test_helper'
 
 class TestSixes < Test::Unit::TestCase
   def test_that_a_throw_with_no_6s_yields_0_score
-    assert_equal 0, Yatzy.new(1, 2, 3, 4, 5).sixes
+    assert_equal 0, sixes(1, 2, 3, 4, 5)
   end
 
   def test_that_a_throw_with_6s_yields_sum_of_only_the_6s
-    assert_equal 6, Yatzy.new(6, 1, 2, 3, 4).sixes
-    assert_equal 12, Yatzy.new(6, 6, 1, 2, 4).sixes
-    assert_equal 18, Yatzy.new(6, 6, 6, 1, 3).sixes
-    assert_equal 24, Yatzy.new(6, 6, 6, 6, 3).sixes
-    assert_equal 30, Yatzy.new(6, 6, 6, 6, 6).sixes
+    assert_equal 6, sixes(6, 1, 2, 3, 4)
+    assert_equal 12, sixes(6, 6, 1, 2, 4)
+    assert_equal 18, sixes(6, 6, 6, 1, 3)
+    assert_equal 24, sixes(6, 6, 6, 6, 3)
+    assert_equal 30, sixes(6, 6, 6, 6, 6)
+  end
+
+  def sixes(*dice)
+    Yatzy.new(*dice).sixes
   end
 end
