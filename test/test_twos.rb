0f5a6b724b7cc42407bddb2c7638a22f0c921324
Hemal <h.n.varambhia@gmail.com>
2016-03-08 13:34:59 +0000

    Extracted an invocation method

--- test/test_twos.rb
+++ test/test_twos.rb
@@ -2,14 +2,20 @@ require_relative 'test_helper'
 
 class TestTwos < Test::Unit::TestCase
   def test_that_a_throw_with_no_2s_yields_0_score
-    assert_equal 0, Yatzy.new(1, 1, 3, 1, 6).twos
+    assert_equal 0, twos(1, 1, 3, 1, 6)
   end
 
   def test_that_a_throw_with_2s_yields_the_sum_of_only_the_2s
-    assert_equal 2, Yatzy.new(2, 1, 3, 4, 6).twos
-    assert_equal 4, Yatzy.new(2, 2, 1, 3, 3).twos
-    assert_equal 6, Yatzy.new(2, 2, 2, 3, 3).twos
-    assert_equal 8, Yatzy.new(2, 2, 2, 2, 3).twos
-    assert_equal 10, Yatzy.new(2, 2, 2, 2, 2).twos
+    assert_equal 2, twos(2, 1, 3, 4, 6)
+    assert_equal 4, twos(2, 2, 1, 3, 3)
+    assert_equal 6, twos(2, 2, 2, 3, 3)
+    assert_equal 8, twos(2, 2, 2, 2, 3)
+    assert_equal 10, twos(2, 2, 2, 2, 2)
+  end
+
+  private
+
+  def twos(*dice)
+    Yatzy.new(*dice).twos
   end
 end
