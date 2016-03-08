a10e2ca0e3aa1cf0cd4792fdfd2069dc328cead3
Hemal <h.n.varambhia@gmail.com>
2016-03-08 12:26:42 +0000

    Extracted an invocation method

--- test/test_fives.rb
+++ test/test_fives.rb
@@ -2,14 +2,18 @@ require_relative 'test_helper'
 
 class TestFives < Test::Unit::TestCase
   def test_that_a_throw_with_no_fives_yields_0_score
-    assert_equal 0, Yatzy.new(1, 2, 3, 3, 4).fives
+    assert_equal 0, fives(1, 2, 3, 3, 4)
   end
 
   def test_that_a_throw_with_5s_yields_sum_of_only_the_5s
-    assert_equal 5, Yatzy.new(5, 2, 3, 1, 4).fives
-    assert_equal 10, Yatzy.new(5, 5, 1, 2, 6).fives
-    assert_equal 15, Yatzy.new(5, 5, 5, 2, 4).fives
-    assert_equal 20, Yatzy.new(5, 5, 5, 5, 4).fives
-    assert_equal 25, Yatzy.new(5, 5, 5, 5, 5).fives
+    assert_equal 5, fives(5, 2, 3, 1, 4)
+    assert_equal 10, fives(5, 5, 1, 2, 6)
+    assert_equal 15, fives(5, 5, 5, 2, 4)
+    assert_equal 20, fives(5, 5, 5, 5, 4)
+    assert_equal 25, fives(5, 5, 5, 5, 5)
+  end
+
+  def fives(*dice)
+    Yatzy.new(*dice).fives
   end
 end
