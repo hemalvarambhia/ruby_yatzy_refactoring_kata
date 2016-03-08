06c778990cc397a5f70433cd89c1e678fee056d0
Hemal <h.n.varambhia@gmail.com>
2016-03-08 13:32:14 +0000

    Extracted an invocation method

--- test/test_two_pairs.rb
+++ test/test_two_pairs.rb
@@ -2,19 +2,25 @@ require_relative 'test_helper'
 
 class TestTwoPairs < Test::Unit::TestCase
   def test_that_a_throw_with_no_pairs_yields_0_score
-    assert_equal 0, Yatzy.new(1, 2, 3, 4, 5).two_pair
+    assert_equal 0, two_pair(1, 2, 3, 4, 5)
   end
 
   def test_that_a_throw_with_just_one_pair_yields_0_score
-    assert_equal 0, Yatzy.new(1, 2, 2, 3, 4).two_pair
+    assert_equal 0, two_pair(1, 2, 2, 3, 4)
   end
 
   def test_that_a_throw_with_two_pairs_both_same_numbers_yields_0_score
-    assert_equal 0, Yatzy.new(5, 5, 5, 5, 1).two_pair
+    assert_equal 0, two_pair(5, 5, 5, 5, 1)
   end
 
   def test_a_throw_with_two_pairs_sums_the_highest_two_pairs
-    assert_equal 16, Yatzy.new(1, 5, 5, 3, 3).two_pair
-    assert_equal 16, Yatzy.new(3, 3, 5, 5, 5).two_pair
+    assert_equal 16, two_pair(1, 5, 5, 3, 3)
+    assert_equal 16, two_pair(3, 3, 5, 5, 5)
+  end
+
+  private
+
+  def two_pair(*dice)
+    Yatzy.new(*dice).two_pair
   end
 end
