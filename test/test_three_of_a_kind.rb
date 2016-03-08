e441773939ead830fb58d8d0ebef3ca69ef75a5e
Hemal <h.n.varambhia@gmail.com>
2016-03-08 13:27:45 +0000

    Extracted an invocation method

--- test/test_three_of_a_kind.rb
+++ test/test_three_of_a_kind.rb
@@ -2,15 +2,21 @@ require_relative 'test_helper'
 
 class TestThreeOfAKind < Test::Unit::TestCase
   def test_that_a_throw_with_no_3_of_a_kind_yields_0_score
-    assert_equal 0, Yatzy.new(1, 2, 3, 4, 5).three_of_a_kind
+    assert_equal 0, three_of_a_kind(1, 2, 3, 4, 5)
   end
   
   def test_that_a_throw_with_a_pair_yields_0_score
-    assert_equal 0, Yatzy.new(1, 2, 2, 4, 5).three_of_a_kind
+    assert_equal 0, three_of_a_kind(1, 2, 2, 4, 5)
   end
 
   def test_that_a_throw_with_3_of_a_kind_sums_only_those_3_faces
-    assert_equal 15, Yatzy.new(1, 5, 5, 5, 2).three_of_a_kind
-    assert_equal 15, Yatzy.new(5, 5, 5, 5, 2).three_of_a_kind
+    assert_equal 15, three_of_a_kind(1, 5, 5, 5, 2)
+    assert_equal 15, three_of_a_kind(5, 5, 5, 5, 2)
+  end
+
+  private
+
+  def three_of_a_kind(*dice)
+    Yatzy.new(*dice).three_of_a_kind
   end
 end
