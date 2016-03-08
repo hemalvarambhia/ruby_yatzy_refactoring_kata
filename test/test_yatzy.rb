9a75bcc4b9525752658953c5687a2a9f0c1ec429
Hemal <h.n.varambhia@gmail.com>
2016-03-08 13:36:58 +0000

    Extracted an invocation method

--- test/test_yatzy.rb
+++ test/test_yatzy.rb
@@ -2,12 +2,18 @@ require_relative 'test_helper'
 
 class YatzyTest < Test::Unit::TestCase
   def test_yatzy_scores_50
-    assert_equal 50, Yatzy.new(6, 6, 6, 6, 6).yatzy
-    assert_equal 50, Yatzy.new(2, 2, 2, 2, 2).yatzy 
-    assert_equal 50, Yatzy.new(3, 3, 3, 3, 3).yatzy
+    assert_equal 50, yatzy(6, 6, 6, 6, 6)
+    assert_equal 50, yatzy(2, 2, 2, 2, 2)
+    assert_equal 50, yatzy(3, 3, 3, 3, 3)
   end
 
   def test_no_yatzy_scores_0
-    assert_equal 0, Yatzy.new(6, 6, 6, 6, 3).yatzy
+    assert_equal 0, yatzy(6, 6, 6, 6, 3)
+  end
+
+  private
+
+  def yatzy(*dice)
+    Yatzy.new(*dice).yatzy
   end
 end
