8ff354ffefcd951f7c97a31533a681b4c66a74ca
Hemal <h.n.varambhia@gmail.com>
2016-03-08 13:30:02 +0000

    Extracted an invocation method

--- test/test_threes.rb
+++ test/test_threes.rb
@@ -2,14 +2,20 @@ require_relative 'test_helper'
 
 class TestThrees < Test::Unit::TestCase
   def test_that_a_throw_with_no_3s_yields_0_score
-    assert_equal 0, Yatzy.new(1, 2, 4, 5, 6).threes
+    assert_equal 0, threes(1, 2, 4, 5, 6)
   end
 
   def test_that_a_throw_with_3s_yields_sum_of_only_the_3s
-    assert_equal 3, Yatzy.new(3, 2, 1, 2, 4).threes
-    assert_equal 6, Yatzy.new(3, 3, 1, 2, 4).threes
-    assert_equal 9, Yatzy.new(3, 3, 3, 2, 1).threes
-    assert_equal 12, Yatzy.new(3, 3, 3, 3, 2).threes
-    assert_equal 15, Yatzy.new(3, 3, 3, 3, 3).threes
+    assert_equal 3, threes(3, 2, 1, 2, 4)
+    assert_equal 6, threes(3, 3, 1, 2, 4)
+    assert_equal 9, threes(3, 3, 3, 2, 1)
+    assert_equal 12, threes(3, 3, 3, 3, 2)
+    assert_equal 15, threes(3, 3, 3, 3, 3)
+  end
+
+  private
+
+  def threes(*dice)
+    Yatzy.new(*dice).threes
   end
 end
