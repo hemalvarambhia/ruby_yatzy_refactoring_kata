ea1dd2cdc3cbad122c3944f22eba6385de580cc8
Hemal <h.n.varambhia@gmail.com>
2016-03-08 11:42:55 +0000

    Include Enumerable to get the methods for free

--- lib/roll.rb
+++ lib/roll.rb
@@ -1,17 +1,22 @@
 class Roll
   extend Forwardable
+  include Enumerable
   def_delegators :@dice, :uniq, :count, :sort
 
   def initialize(dice)
     @dice = dice
     @tally = Hash[
-        @dice.uniq.collect { |face| [face, @dice.count(face)] }
+        uniq.collect { |face| [face, count(face)] }
     ]
+  end
+
+  def each &block
+    @dice.each &block
   end
 
   def add_up
     to_add_up = @dice
-    to_add_up = @dice.select { |face| yield face } if block_given?
+    to_add_up = select { |face| yield face } if block_given?
     to_add_up.reduce(0, :+)
   end
 
