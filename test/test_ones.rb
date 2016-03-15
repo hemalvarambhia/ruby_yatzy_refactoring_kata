6f35bab167c00483a5361a699e59096b942b9365
hnvarambhia <h.n.varambhia@gmail.com>
2016-03-15 12:12:26 +0000

    Removed redundant tests, one another whether order changed

--- test/test_ones.rb
+++ test/test_ones.rb
@@ -9,9 +9,7 @@ class TestOnes < Test::Unit::TestCase
   def test_that_a_throw_with_1s_yields_the_sum_of_only_the_1s
     assert_equal 1, ones(1, 2, 3, 4, 5)
     assert_equal 2, ones(1, 1, 3, 4, 5)
-    assert_equal 3, ones(1, 1, 1, 4, 3)
-    assert_equal 4, ones(1, 1, 1, 1, 6)
-    assert_equal 5, ones(1, 1, 1, 1, 1)
+    assert_equal 3, ones(1, 1, 3, 4, 1)
   end
 
   private
