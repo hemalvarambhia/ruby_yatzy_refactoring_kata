737684fc198592a0ceaf902ca805227d85cbf0c5
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-27 20:06:00 +0100

    This is tested in the Roll test case

--- test/test_ones.rb
+++ test/test_ones.rb
@@ -2,10 +2,6 @@ require_relative 'test_helper'
 require_relative '../lib/yatzy'
 
 class TestOnes < Test::Unit::TestCase
-  def test_that_a_throw_with_no_ones_yields_0_score
-    assert_equal 0, ones(6, 2, 2, 4, 5)
-  end
-
   def test_that_a_throw_with_1s_yields_the_sum_of_only_the_1s
     assert_equal 1, ones(1, 2, 3, 4, 5)
     assert_equal 2, ones(1, 1, 3, 4, 5)
