5b32c3cabeef0ee22f79e6b1c9b5f160bb9b3324
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-27 20:07:19 +0100

    This is tested in the Roll test case

--- test/test_twos.rb
+++ test/test_twos.rb
@@ -1,10 +1,6 @@
 require_relative 'test_helper'
 
 class TestTwos < Test::Unit::TestCase
-  def test_that_a_throw_with_no_2s_yields_0_score
-    assert_equal 0, twos(1, 1, 3, 1, 6)
-  end
-
   def test_that_a_throw_with_2s_yields_the_sum_of_only_the_2s
     assert_equal 2, twos(2, 1, 3, 4, 6)
     assert_equal 4, twos(2, 2, 1, 3, 3)
