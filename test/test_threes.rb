60f570626cc3f77af5ca0ab937cd2ad5aaa917ad
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-27 20:01:55 +0100

    Removed redundant tests, added one where the order was altered

--- test/test_threes.rb
+++ test/test_threes.rb
@@ -6,11 +6,9 @@ class TestThrees < Test::Unit::TestCase
   end
 
   def test_that_a_throw_with_3s_yields_sum_of_only_the_3s
-    assert_equal 3, threes(3, 2, 1, 2, 4)
-    assert_equal 6, threes(3, 3, 1, 2, 4)
-    assert_equal 9, threes(3, 3, 3, 2, 1)
-    assert_equal 12, threes(3, 3, 3, 3, 2)
-    assert_equal 15, threes(3, 3, 3, 3, 3)
+    assert_equal 6, threes(3, 3, 5, 5, 5)
+    assert_equal 9, threes(3, 3, 3, 2, 2)
+    assert_equal 12, threes(3, 2, 3, 3, 3)
   end
 
   private
