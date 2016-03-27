ba9fc0c228b640e9f3c282624d0f8d5290788350
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-27 20:04:12 +0100

    Added a missing test assertion

--- test/test_roll.rb
+++ test/test_roll.rb
@@ -4,5 +4,6 @@ class TestRoll < Test::Unit::TestCase
   def test_that_we_only_sum_the_faces_the_dice_landed_on
     roll = Roll.new([1, 2, 6, 6, 2])
     assert_equal roll.add_up { |landed| landed == 2 }, 4
+    assert_equal roll.add_up { |landed| landed == 3 }, 0
   end
 end
