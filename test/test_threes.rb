f928681b5e9c8cc520a9b979eed8c0ada158e23a
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-27 20:08:36 +0100

    This is tested in the Roll test case

--- test/test_threes.rb
+++ test/test_threes.rb
@@ -1,10 +1,6 @@
 require_relative 'test_helper'
 
 class TestThrees < Test::Unit::TestCase
-  def test_that_a_throw_with_no_3s_yields_0_score
-    assert_equal 0, threes(1, 2, 4, 5, 6)
-  end
-
   def test_that_a_throw_with_3s_yields_sum_of_only_the_3s
     assert_equal 6, threes(3, 3, 5, 5, 5)
     assert_equal 9, threes(3, 3, 3, 2, 2)
