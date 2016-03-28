3b0026cf1c50967cbfbc4b525798a8f562e8cbdd
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-28 15:52:39 +0100

    This scenario is tested in the Roll test case

--- test/test_fives.rb
+++ test/test_fives.rb
@@ -1,10 +1,6 @@
 require_relative 'test_helper'
 
 class TestFives < Test::Unit::TestCase
-  def test_that_a_throw_with_no_fives_yields_0_score
-    assert_equal 0, fives(1, 2, 3, 3, 4)
-  end
-
   def test_that_a_throw_with_5s_yields_sum_of_only_the_5s
     assert_equal 5, fives(5, 2, 3, 1, 4)
     assert_equal 10, fives(5, 5, 1, 2, 6)
