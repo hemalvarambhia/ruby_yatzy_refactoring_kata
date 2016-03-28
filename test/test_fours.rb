7f67e3a093bc973eb46560e65c444b3a5959f151
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-28 15:51:27 +0100

    This scenario is tested in the Roll test case

--- test/test_fours.rb
+++ test/test_fours.rb
@@ -1,10 +1,6 @@
 require_relative 'test_helper'
 
 class TestFours < Test::Unit::TestCase
-  def test_that_a_throw_with_no_4s_yields_0_score
-    assert_equal 0, fours(1, 2, 3, 5, 6)
-  end
-    
   def test_that_a_throw_with_4s_yields_a_sum_of_only_the_4s
     assert_equal 12, fours(4, 4, 4, 5, 5)
     assert_equal 8, fours(4, 4, 5, 5, 5)
