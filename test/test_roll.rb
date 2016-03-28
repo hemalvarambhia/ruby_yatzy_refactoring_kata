0b9ed73ac3a4589a6bb2e3d8f6bb1ee16e39916a
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-28 16:11:16 +0100

    Corrected argument ordering

--- test/test_roll.rb
+++ test/test_roll.rb
@@ -3,8 +3,8 @@ require_relative 'test_helper'
 class TestRoll < Test::Unit::TestCase
   def test_that_we_only_sum_the_faces_the_dice_landed_on
     roll = Roll.new([1, 2, 6, 6, 2])
-    assert_equal roll.add_up { |landed| landed == 2 }, 4
-    assert_equal roll.add_up { |landed| landed == 3 }, 0
+    assert_equal 4, roll.add_up { |landed| landed == 2 }
+    assert_equal 0, roll.add_up { |landed| landed == 3 }
   end
 
   def test_that_find_dice_that_meet_count_criteria
