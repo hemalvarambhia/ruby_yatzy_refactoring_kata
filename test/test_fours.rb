d07eabfa08ab022b54c1b1e86835a5f443629bb5
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-28 15:57:18 +0100

    Removed redundant tests, changed the order of the dice faces in the last test

--- test/test_fours.rb
+++ test/test_fours.rb
@@ -2,10 +2,9 @@ require_relative 'test_helper'
 
 class TestFours < Test::Unit::TestCase
   def test_that_a_throw_with_4s_yields_a_sum_of_only_the_4s
-    assert_equal 12, fours(4, 4, 4, 5, 5)
+    assert_equal 4, fours(4, 1, 5, 5, 5)
     assert_equal 8, fours(4, 4, 5, 5, 5)
-    assert_equal 4, fours(4, 5, 5, 5, 5)
-    assert_equal 16, fours(4, 4, 4, 4, 1)
+    assert_equal 12, fours(4, 4, 5, 5, 4)
   end
 
   private
