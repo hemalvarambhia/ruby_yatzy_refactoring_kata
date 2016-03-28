2ed49548e4428e0e2d4318b7c51832c0ce1c8f3f
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-28 16:01:40 +0100

    This scenario is tested in the Roll test case

--- test/test_sixes.rb
+++ test/test_sixes.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class TestSixes < Test::Unit::TestCase
-  def test_that_a_throw_with_no_6s_yields_0_score
-    assert_equal 0, sixes(1, 2, 3, 4, 5)
-  end
-
   def test_that_a_throw_with_6s_yields_sum_of_only_the_6s
     assert_equal 6, sixes(6, 1, 2, 3, 4)
     assert_equal 12, sixes(6, 6, 1, 2, 4)
