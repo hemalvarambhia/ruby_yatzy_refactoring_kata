395f4df91a287683dd964de0611844f082f4260e
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-28 15:59:28 +0100

    Removed redundant tests, changed the order of the dice faces in the last test

--- test/test_fives.rb
+++ test/test_fives.rb
@@ -3,10 +3,8 @@ require_relative 'test_helper'
 class TestFives < Test::Unit::TestCase
   def test_that_a_throw_with_5s_yields_sum_of_only_the_5s
     assert_equal 5, fives(5, 2, 3, 1, 4)
-    assert_equal 10, fives(5, 5, 1, 2, 6)
     assert_equal 15, fives(5, 5, 5, 2, 4)
-    assert_equal 20, fives(5, 5, 5, 5, 4)
-    assert_equal 25, fives(5, 5, 5, 5, 5)
+    assert_equal 20, fives(5, 5, 4, 5, 5)
   end
 
   private
