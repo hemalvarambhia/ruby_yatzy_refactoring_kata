9918247ac5536ffbb280dd1238c5c44350c3e379
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-03-28 16:03:52 +0100

    Removed redundant tests, changed the order of the dice faces in the last test to demonstrate order invariance

--- test/test_sixes.rb
+++ test/test_sixes.rb
@@ -3,10 +3,8 @@ require 'test_helper'
 class TestSixes < Test::Unit::TestCase
   def test_that_a_throw_with_6s_yields_sum_of_only_the_6s
     assert_equal 6, sixes(6, 1, 2, 3, 4)
-    assert_equal 12, sixes(6, 6, 1, 2, 4)
     assert_equal 18, sixes(6, 6, 6, 1, 3)
-    assert_equal 24, sixes(6, 6, 6, 6, 3)
-    assert_equal 30, sixes(6, 6, 6, 6, 6)
+    assert_equal 24, sixes(6, 3, 6, 6, 6)
   end
 
   private
